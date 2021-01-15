da35bb9c4171ae0c081973527e223069d0a9149a
Luk1Pr0 <lpytel16@gmail.com>
2021-01-15 20:42:22 +0100

    Fetching users from github api and rendering them out

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import Navbar from './Components/Layout/Navbar';
 import Users from './Components/Users/Users';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch('https://api.github.com/users')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ users: data, loading: false })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <div className="container">
-          <Users />
+        <div className="container" style={{ paddingTop: '4rem' }}>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,32 +2,8 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 class Users extends Component {
-
-    state = {
-        users: [
-            {
-                "login": "kevinclark",
-                "id": 20,
-                "avatar_url": "https://avatars3.githubusercontent.com/u/20?v=4",
-                "html_url": "https://github.com/kevinclark",
-            },
-            {
-                "login": "technoweenie",
-                "id": 21,
-                "avatar_url": "https://avatars3.githubusercontent.com/u/21?v=4",
-                "html_url": "https://github.com/technoweenie",
-            },
-            {
-                "login": "macournoyer",
-                "id": 22,
-                "avatar_url": "https://avatars3.githubusercontent.com/u/22?v=4",
-                "html_url": "https://github.com/macournoyer",
-            }
-        ]
-    }
-
     render() {
-        const { users } = this.state;
+        const { users } = this.props;
         return (
             <div style={userStyle}>
                 {users.map(user => (
