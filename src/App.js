78056f92242418a825c463cb9d18f1d8366002b2
Luk1Pr0 <lpytel16@gmail.com>
2021-01-19 10:52:45 +0100

    Added github repos to the user component

--- src/App.js
+++ src/App.js
@@ -10,75 +10,85 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    users: [],
-    user: {},
-    loading: false,
-    alert: null,
-  };
+    state = {
+        repos: [],
+        users: [],
+        user: {},
+        loading: false,
+        alert: null,
+    };
 
-  // Search Github users
-  searchUsers = (text) => {
-    this.setState({ loading: true });
-    fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-      .then(resp => resp.json())
-      .then(data => this.setState({ users: data.items, loading: false }))
-      .catch(err => console.log(err));
-  };
+    // Search Github users
+    searchUsers = (text) => {
+        this.setState({ loading: true });
+        fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+            .then(resp => resp.json())
+            .then(data => this.setState({ users: data.items, loading: false }))
+            .catch(err => console.log(err));
+    };
 
 
-  // Get single github user
-  getUser = (username) => {
-    this.setState({ loading: true });
-    fetch(`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-      .then(resp => resp.json())
-      .then(data => this.setState({ user: data, loading: false }))
-      .catch(err => console.log(err));
-  }
+    // Get single github user
+    getUser = (username) => {
+        this.setState({ loading: true });
+        fetch(`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+            .then(resp => resp.json())
+            .then(data => this.setState({ user: data, loading: false }))
+            .catch(err => console.log(err));
+    }
 
-  // Clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
+    // Get User repositoeries
+    getUserRepos = (username) => {
+        this.setState({ loading: true });
+        fetch(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:dsc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+            .then(resp => resp.json())
+            .then(data => this.setState({ repos: data, loading: false }))
+            .catch(err => console.log(err));
+    }
 
-  // Set alert
-  setAlert = (message, type) => {
-    this.setState({ alert: { message, type } });
-    setTimeout(() => this.setState({ alert: null }), 3000);
-  }
+    // Clear users from state
+    clearUsers = () => this.setState({ users: [], loading: false });
 
-  render() {
-    const { user, users, loading } = this.state;
-    return (
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={this.getUser} user={user} loading={loading} />
-              )} />
-            </Switch>
-            <Alert alert={this.state.alert} />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+    // Set alert
+    setAlert = (message, type) => {
+        this.setState({ alert: { message, type } });
+        setTimeout(() => this.setState({ alert: null }), 3000);
+    }
+
+    render() {
+        const { user, users, loading, repos } = this.state;
+        return (
+            <Router>
+                <div className='App'>
+                    <Navbar />
+                    <div className="container">
+                        <Switch>
+                            <Route
+                                exact
+                                path='/'
+                                render={props => (
+                                    <Fragment>
+                                        <Search
+                                            searchUsers={this.searchUsers}
+                                            clearUsers={this.clearUsers}
+                                            showClear={users.length > 0 ? true : false}
+                                            setAlert={this.setAlert}
+                                        />
+                                        <Users loading={loading} users={users} />
+                                    </Fragment>
+                                )}
+                            />
+                            <Route exact path='/about' component={About} />
+                            <Route exact path='/user/:login' render={props => (
+                                <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading} />
+                            )} />
+                        </Switch>
+                        <Alert alert={this.state.alert} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Spinner from '../Layout/Spinner';
+import Repos from '../Repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -7,12 +8,15 @@ export class User extends Component {
 
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
+        repos: PropTypes.array.isRequired,
     }
 
     render() {
@@ -31,7 +35,7 @@ export class User extends Component {
             hireable
         } = this.props.user;
 
-        const { loading } = this.props;
+        const { loading, repos } = this.props;
 
         return (
             loading ? <Spinner /> : (
@@ -65,7 +69,8 @@ export class User extends Component {
                         <div className="badge badge-light">Public Repos: {public_repos}</div>
                         <div className="badge badge-dark">Public Gists: {public_gists}</div>
                     </div>
-                </div >
+                    <Repos repos={repos} />
+                </div>
             )
         )
     }
